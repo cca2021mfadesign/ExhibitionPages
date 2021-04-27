72a075be5aef3289447dcb8bbad01ee8ece671ba
Xiaohan Liao <liao.xiaohan@cca.edu>
2021-04-27 12:30:32 -0700

    add demo3

---
+++ js/demo3-map.js
@@ -0,0 +1,24 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHVzdHN0YXIwMjEzIiwiYSI6ImNrbnhpcDBreTBtajcyb3FkdTY1cnd1Z2QifQ.pUKdIHe_quvCGrAsPnh65g'; // need to change
+var hannahlocation = [37.7690643, -122.4033639]; // my current location
+var map = new mapboxgl.Map({
+  container: 'map', // container id
+  style: 'mapbox://styles/duststar0213/cko0eadj105ge17o12dgwbik7', // custom style url from https://studio.mapbox.com/
+  center: hannahlocation, // starting position
+  zoom: 15 // starting zoom
+});
+
+var popup = new mapboxgl.Popup({
+  offset: 25
+}).setText(
+  'xiaohan liao'
+);
+
+// create DOM element for the marker
+var el = document.createElement('div');
+el.id = 'marker';
+
+// create the marker
+new mapboxgl.Marker(el)
+  .setLngLat(hannahlocation)
+  .setPopup(popup) // sets a popup on this marker
+  .addTo(map);
