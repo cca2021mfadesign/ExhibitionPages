027dc03e94b85e7fa052f142babb1914bb9fa1d3
Xiaohan Liao <liao.xiaohan@cca.edu>
2021-05-04 11:48:54 -0700

    improved landinpage
    -add images gallery as background
    -clickable names and stop names while hover

--- demo1_improved/sketch.js
+++ demo1_improved/sketch.js
@@ -1,18 +1,24 @@
 // let names = ["Keyu Mao", "Sarah Bonnickson", "Abigayle Cosinuke", "Tingyu Zhao", "Kaiwen Liu", "Fuming Zhou", "Yashan Deng", "Qianshi Zheng", "Jason Malik", "Mengwen Yu", "Eryn Bathke", "Tiaoli Li", "Haojie Chai", "Xiaohan Hannah Liao", "Hancai Wang", "Wen Liu", "Ashlyn Jackson", "Chloe Looker", "Troy Taylor", 
 // "Tin Dinh", "Shang-Te Chen", "Meg Quarton", "Dom Granato", "Danielle Kim", "Rebecca Gardea"];
 
+
 let data;
 let cnv;
 let circlesDiv = [];
 let namesdiv = [];
 let namea;
 let circles;
+let changesColors = false;
+let hoverNames;
+let idstring;
+let particleIsStop = false;
+let cnvW,cn
 class Particle {
   // setting the co-ordinates, radius and the
   // speed of a particle in both the co-ordinates axes.
   constructor() {
-    this.x = random(40, windowWidth - 30);
-    this.y = random(40, windowHeight - 30);
+    this.x = random(40, width - 30);
+    this.y = random(40, height - 30);
     this.r = 15;
     this.xSpeed = random(-1.5, 2);
     this.ySpeed = random(-1.5, 1.5);
@@ -30,12 +36,18 @@ class Particle {
 
   // setting the particle in motion.
   moveParticle() {
-    if (this.x < windowWidth / 10 || this.x > windowWidth)
+    if (this.x < width / 10 || this.x > width)
       this.xSpeed *= -1;
-    if (this.y < windowHeight / 10 || this.y > windowHeight)
+    if (this.y < height / 10 || this.y > height)
       this.ySpeed *= -1;
     this.x += this.xSpeed;
     this.y += this.ySpeed;
+  }
+    stopParticle() {
+    this.x = mouseX;
+    this.y = mouseY;
+    this.xSpeed += 0;
+    this.ySpeed += 0;
   }
 
   // this function creates the connections(lines)
@@ -44,8 +56,8 @@ class Particle {
     particles.forEach(element => {
       let dis = dist(this.x, this.y, element.x, element.y);
       if (dis < 200) {
-        stroke('rgba(255,255,255,0.5)');
-        strokeWeight(0.5);
+        stroke('rgba(0,0,0,0.5)');
+        strokeWeight(1);
         line(this.x, this.y, element.x, element.y);
       }
     });
@@ -61,13 +73,19 @@ function preload() {
 }
 
 function setup() {
-  cnv = createCanvas(windowWidth, windowHeight);
+  //cnv = createCanvas(width, height);
+  let bkcontainer = document.getElementById('background-container')
 
+  cnv = createCanvas(bkcontainer.offsetWidth, bkcontainer.offsetHeight);
+  cnv.parent(bkcontainer);
+ // cnv.style("z-index: 10");
+  cnv.position(0,0);
   for (let i = 0; i < data.getRowCount(); i++) {
     particles.push(new Particle());
       namea = createElement('a');
     circles=createElement('div');
-
+        namea.style('color', '#000');
+    namea.style('font-size', '30px');
     namesdiv.push(namea);
     circlesDiv.push(circles);
    
@@ -84,7 +102,6 @@ function setup() {
   console.log("namesdiv#" + namesdiv.length);
   rectMode(CENTER);
   textFont(myFont);
-  textSize(20);
   console.log(data.getRowCount());
   // what are the columns?
   console.log(data.columns);
@@ -97,24 +114,52 @@ function setup() {
 }
 
 function draw() {
-  background('#0f0f0f');
+ // background('#0f0f0f');
+ //background('rgba(255,255,255, 0.001)');
+ clear();
+
 //  background(100);
+  //  background(100);
   for (let i = 0; i < particles.length; i++) {
-    let c = color(str(data.getColumn('colors')[i]));
+
     // fill(c);
+    let c = color(str(data.getColumn('colors')[i]));
+//     particles[i].createParticle(data.getColumn('names')[i], c);
+
+//     particles[i].moveParticle();
+//     particles[i].joinParticles(particles.slice(i));
+
+
+//     //  namesdiv[i].style('color','#fff');
+
+//     circlesDiv[i].addClass('circles');
+//     circlesDiv[i].position(particles[i].x - 10, particles[i].y)
+//     circlesDiv[i].style('background-color', c);
+//     namesdiv[i].position(particles[i].x, particles[i].y - 20);
+
+       document.getElementById(str(data.getColumn('names')[i])).onmouseover = function() {
+      document.getElementById(str(data.getColumn('names')[i])).style.color = c;
+      particles[i].stopParticle();
+    };
+    document.getElementById(str(data.getColumn('names')[i])).onmouseout = function() {
+    document.getElementById(str(data.getColumn('names')[i])).style.color = '#000';
+    particles[i].moveParticle();
+    };
     particles[i].createParticle(data.getColumn('names')[i], c);
 
     particles[i].moveParticle();
     particles[i].joinParticles(particles.slice(i));
-  
-   namesdiv[i].position(particles[i].x,particles[i].y-20);
-   namesdiv[i].style('color','#fff');
-    namesdiv[i].mouseOver(changeColors);
+
+
+    //  namesdiv[i].style('color','#fff');
+
     circlesDiv[i].addClass('circles');
-  circlesDiv[i].position(particles[i].x-10,particles[i].y)
-  circlesDiv[i].style('background-color',c);
- 
+    circlesDiv[i].position(particles[i].x - 10, particles[i].y)
+    circlesDiv[i].style('background-color', c);
+    namesdiv[i].position(particles[i].x, particles[i].y - 20);
   }
+
+
  
 }
 function changeColors(){
@@ -124,5 +169,5 @@ function changeColors(){
   }
 }
 function windowResized() {
-  cnv = resizeCanvas(windowWidth, windowHeight);
+  cnv = resizeCanvas(width, height);
 }
