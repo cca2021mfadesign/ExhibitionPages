64b9f05f97c16c8bec8ed35061f38e068e50c07d
Xiaohan Liao <liao.xiaohan@cca.edu>
2021-05-11 18:02:09 -0700

    add image gallery

--- demo1_improved/isometric/isometricFunction.js
+++ demo1_improved/isometric/isometricFunction.js
@@ -19,11 +19,12 @@ btnText[i].addEventListener('click', function(){
     samplediv[i].style.animationDuration = "1s";
     dots[i].style.display = "none";
     moreText[i].style.display = "block";
-    samplediv[i].style.width = "200%";
+    samplediv[i].style.width = "150%";
 
-    samplediv[i].style.height = "200%";
-    lStatementsContents[i].style.padding = "20px";
+    samplediv[i].style.height = "150%";
+    lStatementsContents[i].style.padding = "30px";
     samplediv[i].style.transform = "rotate(0)";
+    samplediv[i].style.fontSize ="18px";
      samplediv[i].style.zIndex= "10";
 
 });
@@ -40,11 +41,15 @@ btnText[i].addEventListener('click', function(){
     samplediv[i].style.width = "100%";
         samplediv[i].style.height = "100%";
     close[i].style.opacity= 0;
-
+    samplediv[i].style.fontSize ="15px";
+         samplediv[i].style.zIndex= "1";
  
   });
 })(i);
     }
+
+
+
   
  
 
@@ -68,11 +73,12 @@ for(var i =0; i<rbtnText.length; i++){
     rsamplediv[i].style.animationDuration = "1s";
     rdots[i].style.display = "none";
     rmoreText[i].style.display = "block";
-    rsamplediv[i].style.width = "200%";
-      rsamplediv[i].style.height= "200%";
-    rStatementsContents[i].style.padding = "20px";
+    rsamplediv[i].style.width = "150%";
+      rsamplediv[i].style.height= "150%";
+    rStatementsContents[i].style.padding = "30px";
     rsamplediv[i].style.transform = "rotate(0)";
         rsamplediv[i].style.zIndex = "10";
+            rsamplediv[i].style.fontSize ="18px";
     });
 
   })(i);
@@ -86,13 +92,63 @@ for(var i =0; i<rbtnText.length; i++){
     rsamplediv[i].style.animationDuration = "1s";
     rbtnText[i].innerHTML = "MORE"; 
     rsamplediv[i].style.width = "100%";
-    rclose[i].style.opacity= 0;
     rsamplediv[i].style.transform = "scale(1) rotateZ(-60deg) skewY(30deg)"; 
         rsamplediv[i].style.zIndex = "0";
   rsamplediv[i].style.height = "100%";
- 
+      rsamplediv[i].style.fontSize ="15px";
+     rclose[i].style.opacity= 0;
+      rsamplediv[i].style.zIndex = "1";
+
   });
 
   })(i)
 }
+
+
+// function openModal() {
+//   document.getElementById("myModal").style.display = "block";
+// }
+
+// function closeModal() {
+//   document.getElementById("myModal").style.display = "none";
+// }
+
+
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("demo");
+//   var captionText = document.getElementById("caption");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+//   captionText.innerHTML = dots[slideIndex-1].alt;
+// }
+
+// var abigaleImages1 = document.getElementById('abigaleImages1');
+//   abigaleImages1.addEventListener('click', function () {
+//  document.body.style.backgroundColor = "red";
+//   //  galleries[i].style.opacity=1;
+   
+// });
+
 
---
+++ demo1_improved/isometric/openModal.js
@@ -0,0 +1,36 @@
+
+
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function openModal1(){  document.getElementById("myModal1").style.display = "block"; }	function closeModal1(){  document.getElementById("myModal1").style.display = "none";}
+function openModal2(){document.getElementById("myModal2").style.display = "block";  }	function closeModal2( ){ document.getElementById("myModal2").style.display = "none";}
+function openModal3(){document.getElementById("myModal3").style.display = "block";  }	function closeModal3( ){document.getElementById("myModal3").style.display = "none"; }
+function openModal4(){document.getElementById("myModal4").style.display = "block";  }	function closeModal4( ){ document.getElementById("myModal4").style.display = "none";}
+function openModal5(){ document.getElementById("myModal5").style.display = "block"; }	function closeModal5( ){ document.getElementById("myModal5").style.display = "none";}
+function openModal6(){document.getElementById("myModal6").style.display = "block";  }	function closeModal6( ){ document.getElementById("myModal6").style.display = "none";}
+function openModal7(){document.getElementById("myModal7").style.display = "block";  }	function closeModal7( ){ document.getElementById("myModal7").style.display = "none";}
+function openModal8(){ document.getElementById("myModal8").style.display = "block"; }	function closeModal8( ){ document.getElementById("myModal8").style.display = "none";}
+function openModal9(){ document.getElementById("myModal9").style.display = "block"; }	function closeModal9( ){document.getElementById("myModal9").style.display = "none"; }
+function openModal10( ){ document.getElementById("myModal10").style.display = "block"; }	function closeModal10( ){ document.getElementById("myModal10").style.display = "none";}
+function openModal11( ){ document.getElementById("myModal11").style.display = "block"; }	function closeModal11( ){ document.getElementById("myModal11").style.display = "none";}
+function openModal12( ){document.getElementById("myModal12").style.display = "block";  }	function closeModal12( ){ document.getElementById("myModal12").style.display = "none";}
+function openModal13( ){ document.getElementById("myModal13").style.display = "block"; }	function closeModal13( ){ document.getElementById("myModal13").style.display = "none";}
+function openModal14( ){ document.getElementById("myModal14").style.display = "block"; }	function closeModal14( ){document.getElementById("myModal14").style.display = "none"; }
+function openModal15( ){document.getElementById("myModal15").style.display = "block";  }	function closeModal15( ){ document.getElementById("myModal15").style.display = "none";}
+function openModal16( ){ document.getElementById("myModal16").style.display = "block"; }	function closeModal16( ){document.getElementById("myModal16").style.display = "none"; }
+function openModal17( ){document.getElementById("myModal17").style.display = "block";  }	function closeModal17( ){ document.getElementById("myModal17").style.display = "none";}
+function openModal18( ){ document.getElementById("myModal18").style.display = "block"; }	function closeModal18( ){document.getElementById("myModal18").style.display = "none"; }
+function openModal19( ){document.getElementById("myModal19").style.display = "block";  }	function closeModal19( ){document.getElementById("myModal19").style.display = "none"; }
+function openModal20( ){ document.getElementById("myModal20").style.display = "block"; }	function closeModal20( ){ document.getElementById("myModal20").style.display = "none";}
+function openModal21( ){document.getElementById("myModal21").style.display = "block";  }	function closeModal21( ){document.getElementById("myModal21").style.display = "none"; }
+function openModal22( ){document.getElementById("myModal22").style.display = "block";  }	function closeModal22( ){document.getElementById("myModal22").style.display = "none"; }
+function openModal23( ){document.getElementById("myModal23").style.display = "block";  }	function closeModal23( ){ document.getElementById("myModal23").style.display = "none";}
+function openModal24( ){document.getElementById("myModal24").style.display = "block";  }	function closeModal24( ){document.getElementById("myModal24").style.display = "none"; }
+function openModal25( ){ document.getElementById("myModal25").style.display = "block"; }	function closeModal25( ){ document.getElementById("myModal25").style.display = "none";}
